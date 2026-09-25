edd827c6cf8439313b713362e5dc0f0ad645e778
dwaipayanray95 <dwaipayanray95@gmail.com>
2026-09-25 18:19:00 +0530

    chore: update project branding to Vesper Cine

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class RawEdgeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Project RawEdge (R-Camera)',
+      title: 'Vesper Cine',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
